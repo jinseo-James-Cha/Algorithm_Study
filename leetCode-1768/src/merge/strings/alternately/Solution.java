a946517224ed5ddbd383ca5f87ca00e48c2f88b6
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-07-26 02:07:07 +0900

    2021-07-26 version2 started for better efficiency

--- leetCode-1768/src/merge/strings/alternately/Solution.java
+++ leetCode-1768/src/merge/strings/alternately/Solution.java
@@ -16,27 +16,49 @@ public class Solution {
 		// Output: "apbqcr"
 		Solution solution = new Solution();
 		System.out.println(solution.mergeAlternately(new String("abc"), new String("pqr")));
+		System.out.println(solution.mergeAlternately(new String("ab"), new String("pqrs")));
+	}
+	
+	// Ver 2
+	// Approach : 런타임 시간과 메모리 사용량 줄이는 리펙토리 진행
+	public String mergeAlternately(String word1, String word2) {
+		String result = "";
+		int minLength = Math.min(word1.length(), word2.length());
+		
+		for(int i = 0; i < minLength; i++) {
+			result += word1.charAt(i);
+			result += word2.charAt(i);
+		}
+		
+		if(word1.length() > word2.length()) {
+			result += word1.substring(minLength);
+		} else if(word1.length() < word2.length()){
+			result += word2.substring(minLength);
+		}
+		
+		return result;
 	}
 	
 	// Ver1
 	// Approach : 두 문자열 중 작은 길이를 구하고 그 수만큼 word1의 문자와 word2의 문자를 하나씩 더한다.
 	// 			  그 다음, 두 문자열 중 긴 문자열을 찾아 마지막에 붙여준다.
-	public String mergeAlternately(String word1, String word2) {
-        
+	// RESULT
+	// Runtime: 7 ms, faster than 10.64%
+	// Memory Usage: 39.2 MB, less than 18.13%
+	/*public String mergeAlternately(String word1, String word2) {
 		String result = "";
 		int minLength = Math.min(word1.length(), word2.length());
 		
 		for(int i = 0; i < minLength; i++) {
-			result.concat("" + word1.charAt(i));
-			System.out.println(result);
+			result += "" + word1.charAt(i) + word2.charAt(i);
 		}
 		
 		if(word1.length() > word2.length()) {
-			result.concat(word1.substring(minLength));
-		}else {
-			result.concat(word2.substring(minLength));
+			result += word1.substring(minLength);
+		} else if(word1.length() < word2.length()){
+			result += word2.substring(minLength);
 		}
 		
 		return result;
-    }
+    }*/
 }
