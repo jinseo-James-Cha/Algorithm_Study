68144a4e67ff6ddcd1f6b8ef573a6896337318f3
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-07-27 16:40:11 +0900

    2021-07-27 approach to HashMap to save values

--- leetCode-1302/src/deepest/leaves/sum/Solution.java
+++ leetCode-1302/src/deepest/leaves/sum/Solution.java
@@ -1,5 +1,8 @@
 package deepest.leaves.sum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
 	public static void main(String[] args) {
@@ -10,26 +13,44 @@ public class Solution {
 		Solution solution = new Solution();
 		
 		// case 1
+		// Input: root = [1,2,3]
+		// Output: 5
+		System.out.println(solution.deepestLeavesSum(new TreeNode(1, // level 0
+				new TreeNode(2),
+				new TreeNode(3))));
+		
+		// case 2
 		// Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
 		// Output: 15
-		System.out.println(solution.deepestLeavesSum(new TreeNode(1, // level 0
-						new TreeNode(2, // level 1
-								new TreeNode(4, // level 2
-										new TreeNode(7), null), // level 3
-								new TreeNode(5, null, null)), // level 2
-						new TreeNode(3, // level 1
-								null, new TreeNode(6, // level 2
-										null, new TreeNode(8)) // level 3
-								)))); // sum level 3 values = 7 + 8
+//		System.out.println(solution.deepestLeavesSum(new TreeNode(1, // level 0
+//						new TreeNode(2, // level 1
+//								new TreeNode(4, // level 2
+//										new TreeNode(7), null), // level 3
+//								new TreeNode(5, null, null)), // level 2
+//						new TreeNode(3, // level 1
+//								null, new TreeNode(6, // level 2
+//										null, new TreeNode(8)) // level 3
+//								)))); // sum level 3 values = 7 + 8
 	}
 	
 	public int deepestLeavesSum(TreeNode root) {
 		
-        int result = 0;
+        int level = 0;
+        Map<Integer, Integer> resultMap = new HashMap<>();
+        TreeNode list = root;
         
+        while(list != null) {
+        	
+        	if(list.left != null) {
+        		resultMap.put(level, list.left.val);
+        	}
+        	if(list.right != null) {
+        		resultMap.put(level, resultMap.get(level) + list.right.val);
+        	}
+        	
+        }
         
-        
-        return result;
+        return resultMap.get(level);
     }
 	
 }
