62524c010ccbfa6ddf0bd095e8e9f43eebaf207c
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-08-25 14:48:16 +0900

    2021-08-25 problem solve started

---
+++ Programmers-WeeklyChallenge-August-week4/src/Solution.java
@@ -0,0 +1,15 @@
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+	public String solution(String[] table, String[] languages, int[] preference) {
+        String answer = "";
+        return answer;
+    }
+	
+}
